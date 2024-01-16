31bea11ed41278980e66564f89daa05808963ca3
fallowShades <fallowshades@gmail.com>
2024-01-16 10:20:41 +0100

    1. cpy pasta time and effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import "./App.css";
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  return <div></div>;
+  const [time, setTime] = useState(new Date().toLocaleTimeString('en'))
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date().toLocaleTimeString('en')
+
+      setTime(now)
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, []) // Empty dependency array ensures useEffect runs only once on mount
+
+  const timeArray = time.split(':')
+  const minutes = parseInt(timeArray[0], 10)
+  const seconds = parseInt(timeArray[1], 10)
+
+  return (
+    <div>
+      {minutes}:{seconds}
+    </div>
+  )
 }
 
-export default App;
+export default App
