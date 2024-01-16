cbca63d1c092d4675b35de2bf1dc13469a624c96
fallowShades <fallowshades@gmail.com>
2024-01-16 11:02:28 +0100

    more timers

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <TimerAssignment4 initialSeconds={300} /> {/* 5 minutes */}
       <TimerAssignment4 initialSeconds={180} /> {/* 3 minutes */}
       <TimerAssignment4 initialSeconds={600} /> {/* 10 minutes */}
-      <TimerAssignment4 initialSeconds={300} />{' '}
+      <TimerAssignment4 initialSeconds={300} /> <Timer initialSeconds={300} />{' '}
       {/* Set initialSeconds to the desired starting time */}
       {/* Add more timers or other components as needed */}
       <Clock city="London" timeZone="Europe/London" />
